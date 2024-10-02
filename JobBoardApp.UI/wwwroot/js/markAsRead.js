50e8f6026e28f721a050991a4530205bced33cf6
mreshboboyev <mreshboboyev@gmail.com>
2024-10-02 18:49:47 +0500

    Notifications : CSS styling

---
+++ JobBoardApp.UI/wwwroot/js/markAsRead.js
@@ -0,0 +1,17 @@
+﻿function markAsRead(id) {
+    fetch(`/notification/MarkAsRead/${id}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                // Reload the page after marking the notification as read
+                location.reload();
+            } else {
+                console.error('Failed to mark notification as read.');
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
