55e33a66e136b36151d983382b02211be63b7c00
mreshboboyev <mreshboboyev@gmail.com>
2024-10-02 11:32:07 +0500

    Notifications : NotificationHub.cs in RealTime folder;

---
+++ JobBoardApp.Infrastructure/RealTime/NotificationHub.cs
@@ -0,0 +1,18 @@
+﻿using JobBoardApp.Application.DTOs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace JobBoardApp.Infrastructure.RealTime
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendNotification(string userId, string message)
+        {
+            await Clients.User(userId).SendAsync("ReceiveNotification", message);
+        }
+
+        public async Task SendNotificationDTO(string userId, NotificationDTO notification)
+        {
+            await Clients.User(userId).SendAsync("ReceiveNotificationDTO", notification);
+        }
+    }
+}
